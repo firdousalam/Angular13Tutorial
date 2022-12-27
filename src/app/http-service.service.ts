b892253c66593f39e6800c3f93b1f78d936e9f7d
firdous alam <firdous.alam2058@gmail.com>
2022-12-27 16:46:59 +0530

    HTTP Client example start setup

---
+++ src/app/http-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class HttpServiceService {
+
+  constructor(public http: HttpClient) { }
+ 
+}
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-server",
+    templateUrl : "./ServerComponent.html"
+})
+
+export class ServerComponent{
+
+}
