3e001a1aa898ebaf06312608d90cfc96a78c124e
firdous alam <firdous.alam2058@gmail.com>
2022-12-28 12:45:09 +0530

    Use HTTP Post Get In Angular

---
+++ src/app/Interface/HttpInterface.ts
@@ -0,0 +1,14 @@
+export interface ReturnPost {
+    "count" : number
+    "entries" : []
+  }
+  export interface LoadData {
+    "API" : string,
+    "Description" : string,
+    "Link" : string,
+    "Auth": string,
+    "HTTPS": boolean,
+    "Cors": string,
+    "Category": string
+
+  }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ReturnPost,LoadData } from  "./Interface/HttpInterface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  public loadedPosts:LoadData[] = [];
+  private url:string = "https://dummy.restapiexample.com/api/v1/create";
+  postHttpData = {"name":"test","salary":"123","age":"23"}
+  getEmployeeUrl = "https://api.publicapis.org/entries";
+  isfetching = true;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: { name: string; salary: number; age: number }) {
+    
     // Send Http request
-    console.log(postData);
+   
+   
+    this.http.post(this.url,this.postHttpData)
+    .subscribe(responseData=>{
+     
+     
+    })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.http.get<ReturnPost>(this.getEmployeeUrl).subscribe(getData=>{
+      console.log(getData);
+      this.loadedPosts = getData.entries;
+      this.isfetching = false;
+    })
+   
   }
 
   onClearPosts() {
